aa524f4fd00c7aff09bc67a4f84e4576f9a35cc1
Mario Adamo Jr <marioadamojr@gmail.com>
2019-03-03 23:50:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+    array.each_with_index do |name, index|
+      puts "#{index + 1}. #{name}"
+    end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
+  array_2 = array.collect do |x|
+    x << "!"
+    x.capitalize
+  end
+  array_2
+end
+
   # Your code here
 end
 
